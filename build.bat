797155ac39004245f67b0fb21b35f0a1b2cdd8dd
Dbug <mike@defence-force.org>
2025-06-22 11:59:33 +0200

    Improved the build script to detect if the ST is available and accepting UIP commands.

--- build.bat
+++ build.bat
@@ -32,20 +32,37 @@ if exist S:\sommarhack\%ATARIPRG% copy %ATARIPRG% S:\sommarhack\%ATARIPRG%
 
 
 :: UIP setup
-:: Launch G:\Network`\uipV310.tos
+:: Launch G:\Network\uipV310.tos
 :: DHCP IP: 192.168.1.128
 :: Can browse the disk content on http://192.168.1.128
 ::
 :: uIP-tools documentation on https://bitbucket.org/sqward/uip-tools/src/master/
 :: Upload file:    curl -0T filename.tos 192.168.1.1/d/filename.tos
 :: Run executable: curl -0 192.168.1.1/c/filename.tos?run="command line"
+:: curl returns code 0 if the request works
+:: curl returns code 2 if the syntax is wrong
+:: curl returns code 28 if it could not connect or timed out
 ::
 SET ATARIP=192.168.1.128
-echo %ESC%[1mUploading Executable to the Atari%ESC%[0m
-bin\curl.exe -0T %ATARIPRG% %ATARIP%/g/sommarhk/%ATARIPRG%
+SET TIMEOUT=0.5
 
-echo %ESC%[1mLaunching Executable on the Atari%ESC%[0m
-bin\curl.exe -0 %ATARIP%/g/sommarhk/%ATARIPRG%?run="command line"
+:: Check if we can access UIP (-s for silent mode, and -m for the timeout value in second)
+curl -s -m %TIMEOUT% -0 %ATARIP%/c?dir >nul
+if %ERRORLEVEL% equ 0 (
+    ECHO.
+    echo %ESC%[1mUploading Executable to the Atari%ESC%[0m
+    bin\curl.exe -0T %ATARIPRG% %ATARIP%/g/sommarhk/%ATARIPRG%
+
+    ECHO.
+    echo %ESC%[1mLaunching Executable on the Atari%ESC%[0m
+    bin\curl.exe -0 %ATARIP%/g/sommarhk/%ATARIPRG%?run="command line"
+) else ( 
+    ECHO.
+    ECHO %ESC%[30;43m%ATARIP% is not reachable: The program was not uploaded to the ST%ESC%[0m
+    ECHO Make sure that the ST is powered on and that G:\Network\uipV310.tos was launched
+    ECHO.
+)
+
 
 
 ::pause
@@ -60,4 +77,8 @@ goto :End
 
 :End
 ECHO done
+ECHO.
+ECHO.
+ECHO.
+ECHO.
 
